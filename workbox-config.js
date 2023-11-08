e7c4c6cdcc7310184d62cb9fc9deedf9619c7682
Alex Stoimenov <astoimenov@despark.com>
2023-11-08 10:48:38 +0200

    Fix build command

--- workbox-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  clientsClaim: true,
-  globDirectory: "./",
-  globPatterns: ["**/*.{js,ico,png,html,json,wav,jpg,mp4,mov,css}"],
-  swDest: "./src/js/sw.js",
-  ignoreURLParametersMatching: [/^utm_/, /^fbclid$/],
-  maximumFileSizeToCacheInBytes: 524000000,
-};
